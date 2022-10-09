50d168e3f6d5d49973fa1ff2154cea18109755f7
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-09 19:50:49 +0200

    Zmiankas

--- src/main/java/tech/zdrzalik/courses/model/AccessLevel/AccessLevelRepository.java
+++ src/main/java/tech/zdrzalik/courses/model/AccessLevel/AccessLevelRepository.java
@@ -1,9 +1,12 @@
 package tech.zdrzalik.courses.model.AccessLevel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+@Repository()
 @Transactional(propagation = Propagation.MANDATORY)
 public interface AccessLevelRepository extends JpaRepository<AccessLevelsEntity, Long>, AccessLevelRepositoryWithEM {
+
 }
