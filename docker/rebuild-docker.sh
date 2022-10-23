e2545ca7e2b27310171533a1a8a617189579436d
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-23 18:40:32 +0200

    fix

--- docker/rebuild-docker.sh
+++ docker/rebuild-docker.sh
@@ -4,4 +4,7 @@ docker compose -f dockercompose.yml down --volumes  --remove-orphans
 
 docker compose -f dockercompose.yml build --no-cache
 
+# Fajnie by to bylo zrobic w kontenerze : (
+mvn clean resources:resources liquibase:update clean -P dev_local -f ../pom.xml
+
 docker compose -f dockercompose.yml up
