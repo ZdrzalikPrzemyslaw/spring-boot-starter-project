5d2d8184d99eb4119cecd28f3634427e56d13f3b
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-23 16:58:11 +0200

    fix access to tomcat admin

---
+++ src/main/java/tech/zdrzalik/courses/controllers/TestController.java
@@ -0,0 +1,17 @@
+package tech.zdrzalik.courses.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller("/test")
+public class TestController {
+
+    @GetMapping(value = "", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody()
+    public String Test() {
+        return "Test Successful";
+    }
+}
