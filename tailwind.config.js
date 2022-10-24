6f9199320c4e11631c4af20f80a06a8abf25bf21
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-24 22:15:51 +0200

    add comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/main/resources/templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-=======
-  mode: process.env.NODE_ENV ? 'jit' : undefined,
-  purge: ["./src/**/*.html", "./src/**/*.js"],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> 7b239077edc9465679560b084f2e35e0fe8d156b
+
