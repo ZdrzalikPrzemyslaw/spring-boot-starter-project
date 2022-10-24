c2eeda4b2f45e22532bc98aebee12e2741d970ec
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-24 22:09:41 +0200

    fix tailwind

--- src/main/java/tech/zdrzalik/courses/controllers/AdminController.java
+++ src/main/java/tech/zdrzalik/courses/controllers/AdminController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller()
 @RequestMapping("/admin")
 public class AdminController {
 
     @GetMapping(value = "", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getAdminPanel() {
-        return new ModelAndView("admin-panel");
+        ModelAndView modelAndView = new ModelAndView("admin-panel");
+        modelAndView.addObject("hello", "world");
+        return modelAndView;
     }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: process.env.NODE_ENV ? 'jit' : undefined,
-  purge: ["./src/**/*.html", "./src/**/*.js"],
-  darkMode: false,
+  content: ["./src/main/resources/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
