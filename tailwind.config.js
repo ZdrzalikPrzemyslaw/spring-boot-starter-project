79f21708e7f10602e6758bad3fc313d9acd34ff1
JuliaSzymanskaFirmao <julia.szymanska@firmao.pl>
2022-10-24 20:40:23 +0200

    Adding wailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
