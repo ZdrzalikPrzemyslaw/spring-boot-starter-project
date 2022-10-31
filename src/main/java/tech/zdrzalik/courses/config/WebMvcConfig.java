f2fb3cfbaf5608d562a85d35b070b30d352b96df
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-31 16:47:02 +0100

    fix change uri

--- src/main/java/tech/zdrzalik/courses/config/WebMvcConfig.java
+++ src/main/java/tech/zdrzalik/courses/config/WebMvcConfig.java
@@ -1,21 +1,25 @@
 package tech.zdrzalik.courses.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.servlet.HttpServletBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import tech.zdrzalik.courses.interceptors.LocaleAttributeChangeInterceptor;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
+    @Autowired
+    private HttpServletRequest httpServletRequest;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LocaleAttributeChangeInterceptor());
