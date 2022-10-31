934144876f85f9a1989bc37357030946e2a590d5
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-10-31 19:23:45 +0100

    fix

--- src/main/java/tech/zdrzalik/courses/exceptions/AccountInfoException.java
+++ src/main/java/tech/zdrzalik/courses/exceptions/AccountInfoException.java
@@ -2,7 +2,7 @@ package tech.zdrzalik.courses.exceptions;
 
 import tech.zdrzalik.courses.common.I18nCodes;
 
-public class AccountInfoException extends AppBaseException{
+public class AccountInfoException extends AppBaseException {
     protected AccountInfoException(String message) {
         super(message);
     }
@@ -11,5 +11,7 @@ public class AccountInfoException extends AppBaseException{
         super(message, cause);
     }
 
-    public static AccountInfoException emailAlreadyExists(){return new AccountInfoException(I18nCodes.EMAIL_EXIST); }
+    public static AccountInfoException emailAlreadyExists() {
+        return new AccountInfoException(I18nCodes.EMAIL_EXIST);
+    }
 }
