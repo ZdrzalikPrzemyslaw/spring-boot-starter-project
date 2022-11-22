1f67f22cc7e5c7d2e334e8591ca3783fa6378df5
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-11-23 00:33:00 +0100

    optimize imports

--- src/main/java/tech/beetwin/stereoscopy/config/LogbookConfig.java
+++ src/main/java/tech/beetwin/stereoscopy/config/LogbookConfig.java
@@ -4,25 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.BodyFilter;
 import org.zalando.logbook.BodyFilters;
-import org.zalando.logbook.Correlation;
-import org.zalando.logbook.DefaultStrategy;
 import org.zalando.logbook.HeaderFilter;
 import org.zalando.logbook.HeaderFilters;
-import org.zalando.logbook.HttpLogWriter;
-import org.zalando.logbook.HttpRequest;
-import org.zalando.logbook.HttpResponse;
-import org.zalando.logbook.Precorrelation;
-import org.zalando.logbook.Sink;
-import org.zalando.logbook.Strategy;
 import org.zalando.logbook.json.JsonBodyFilters;
 
-import javax.annotation.Nullable;
-import java.io.IOException;
 import java.util.Collections;
 
 @Configuration
 public class LogbookConfig {
-
 
     @Bean
     public BodyFilter bodyFilter() {
