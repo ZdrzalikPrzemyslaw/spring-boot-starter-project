63b25b69623b9ed78601cb3bee376932a30975aa
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2022-11-30 19:17:36 +0100

    fix tests

--- src/test/java/tech/beetwin/stereoscopy/AdminControllerTest.java
+++ src/test/java/tech/beetwin/stereoscopy/AdminControllerTest.java
@@ -54,10 +54,10 @@ class AdminControllerTest {
 
     @AfterAll
     static void afterAll(@Autowired JdbcTemplate jdbcTemplate, @Autowired TableMetadataService tableMetadataService) {
-        TestUtils.wipeAuth(SecurityContextHolder.getContext());
+        TestUtils.setAllRolesAuth(SecurityContextHolder.getContext());
         tableMetadataService.wipeAllMetadataCreatedModified();
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "access_levels", "user_info", "account_info", "table_metadata");
-    }
+        TestUtils.wipeAuth(SecurityContextHolder.getContext());}
 
     @BeforeEach
     void setUp() {
